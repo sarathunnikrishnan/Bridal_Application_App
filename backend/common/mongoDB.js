f35a021521829bf7cc7313a07c1245d6319d913b
Sarath Unnikrishnan <sarathunnikrishnan18@gmail.com>
2026-05-24 10:01:00 +0530

    Mongo error fix

--- backend/common/mongoDB.js
+++ backend/common/mongoDB.js
@@ -10,10 +10,7 @@ const connectDB = async () => {
   }
 
   try {
-    const db = await mongoose.connect(`${process.env.MONGODB_URI}/bridal-application`, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    const db = await mongoose.connect(`${process.env.MONGODB_URI}/bridal-application`);
     isConnected = db.connections[0].readyState;
     console.log("MongoDB Connected");
   } catch (error) {
